e7d3bc8aa50b7f0d9766d2a3418743cb7ae1b5b3
Rolexero <rotimiolaolu@gmail.com>
2023-01-21 10:38:00 +0100

    feat: :lipstick: complete the ui for the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import { NextPage } from 'next'
-import tw, { styled } from 'twin.macro'
-import { Header } from '../app/Components/Header'
-import { Banner } from '../app/Components/Banner'
- 
-const Home: NextPage = ()=>{
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { NextPage } from "next";
+import tw, { styled } from "twin.macro";
+import { Header } from "../app/Components/Header";
+import { Banner } from "../app/Components/Banner";
+import { Trending } from "../app/Components/Trending";
+import { Bodycomponent } from "../app/Components/Body";
+
+const Home: NextPage = () => {
   return (
     <Container>
       <Header />
       <Banner />
+      <Trending />
+      <Bodycomponent />
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   ${tw`p-0 `}
 `;
 
-export default Home
-
+export default Home;
